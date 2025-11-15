21f4c4acff5d4b4bdb2cee72a7b901facd1f0046
FreadSt <thiswolfram@gmail.com>
2025-11-15 22:26:06 +0200

    fix product add

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,19 +5,19 @@ import clothes from '../assets/images/clothes.jpg';
 import furniture from '../assets/images/furniture.jpg';
 import toys from '../assets/images/toys.jpg';
 
-import Categorie from './Categorie';
+import Category from './Category.jsx';
 
 const Categories = () => {
   return (
     <section className='p-8' id='categories'>
       <div className='grid gap-2 md:grid-cols-3 mb-2'>
-        <Categorie name='man' image={clothes} />
-        <Categorie name='shoes' image={shoes} />
-        <Categorie name='pants' image={phone} />
+        <Category name='man' image={clothes} />
+        <Category name='shoes' image={shoes} />
+        <Category name='pants' image={phone} />
       </div>
       <div className='grid gap-2 md:grid-cols-2'>
-        <Categorie name='woman' image={furniture} />
-        <Categorie name='sport' image={toys} />
+        <Category name='woman' image={furniture} />
+        <Category name='sport' image={toys} />
       </div>
     </section>
   );
