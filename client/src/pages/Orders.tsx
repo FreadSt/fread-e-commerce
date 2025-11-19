f83b1235acad42f4706a6d493f956df444608b11
FreadSt <thiswolfram@gmail.com>
2025-11-19 20:44:18 +0200

    fix checkout errors and keys mismatch

--- client/src/pages/Orders.tsx
+++ client/src/pages/Orders.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import img from '../assets/images/e-commerce.webp'
 
 const Orders: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <div>Success</div>
-      <button onClick={() => navigate('/')}> Return home</button>
-    </>
+    <main
+        className='min-h-screen relative flex flex-col justify-center items-center '
+        style={{backgroundImage: `url(${img})`, backgroundSize: 'cover'}}
+    >
+      <article className='border gap-6 bg-white p-6 flex flex-col min-w-[200px] max-h-[300px] sm:min-w-[22rem] md:min-w-[25rem] overflow-visible'>
+        <h3 className='font-bold text-3xl align-middle self-center'>Success</h3>
+        <p>Thank you for purchase, we will send shipment info immediatelly</p>
+        <button
+          onClick={() => navigate('/')}
+          className='text-sm lg:text-md cursor-pointer uppercase block p-4 border-2 hover:text-black hover:border-black hover:bg-white bg-black text-white transition ease-out duration-500 disabled:bg-gray-400 disabled:cursor-not-allowed'
+        >
+          Return home
+        </button>
+      </article>
+    </main>
   )
 };
 
